bfef5d34893de294873521839ea405c0558f980f
NathanOkolita <nathan.okolita@gmail.com>
2023-05-11 07:32:21 -0400

    [DOP-1608] Pull images from private crunchy registry using service account.

--- push_and_tag.sh
+++ push_and_tag.sh
@@ -1,3 +1,4 @@
+CRUNCHY_REGISTRY=https://registry.crunchydata.com
 #Grab the images that need to be tagged and then pushed
 CONTROLLER_IMAGE_CLUSTER=$(cat helm/install/values.yaml | shyaml get-value controllerImages.cluster)
 CONTROLLER_IMAGE_UPGRADE=$(cat helm/install/values.yaml | shyaml get-value controllerImages.upgrade)
@@ -7,11 +8,12 @@ RELATED_IMAGES=$(cat helm/install/values.yaml | shyaml values relatedImages imag
 echo $GCR_CREDENTIALS | base64 -d > keyfile.json
 docker login -u _json_key --password-stdin https://gcr.io < keyfile.json
 echo $HARBOR_PASSWORD | docker login -u $HARBOR_USERNAME --password-stdin https://harbor.devops.indico.io
+echo $CRUNCHY_PASSWORD | docker login -u $CRUNCHY_USERNAME --password-stdin $CRUNCHY_REGISTRY
 # pull images
 
 #pull postgres-operator controller image
-docker pull $CONTROLLER_IMAGE_CLUSTER
 IMAGE_BASE=$(basename $CONTROLLER_IMAGE_CLUSTER)
+docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
 #push to gcr
 docker tag $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE
 docker push gcr.io/new-indico/$IMAGE_BASE
@@ -22,8 +24,8 @@ docker push harbor.devops.indico.io/indico/$IMAGE_BASE
 docker rmi $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
 
 #pull postgres-operator upgrade image
-docker pull $CONTROLLER_IMAGE_UPGRADE
 IMAGE_BASE=$(basename $CONTROLLER_IMAGE_UPGRADE)
+docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
 #push to gcr
 docker tag $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE
 docker push gcr.io/new-indico/$IMAGE_BASE
@@ -36,8 +38,8 @@ docker rmi $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE harbor.devops
 for ri in $RELATED_IMAGES;
 do
     IMAGE=$ri
-    docker pull $IMAGE
     IMAGE_BASE=$(basename $IMAGE)
+    docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
     #push to gcr
     docker tag $IMAGE gcr.io/new-indico/$IMAGE_BASE
     docker push gcr.io/new-indico/$IMAGE_BASE
