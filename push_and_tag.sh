531864e50246531acf188c0a9fb1fba8c10910ee
NathanOkolita <nathan.okolita@gmail.com>
2023-05-11 08:19:36 -0400

    [DOP-1608] Fix private crunchy registry variable, pull from correct path.

--- push_and_tag.sh
+++ push_and_tag.sh
@@ -1,4 +1,4 @@
-CRUNCHY_REGISTRY=https://registry.crunchydata.com
+CRUNCHY_REGISTRY=registry.crunchydata.com
 #Grab the images that need to be tagged and then pushed
 CONTROLLER_IMAGE_CLUSTER=$(cat helm/install/values.yaml | shyaml get-value controllerImages.cluster)
 CONTROLLER_IMAGE_UPGRADE=$(cat helm/install/values.yaml | shyaml get-value controllerImages.upgrade)
@@ -8,45 +8,48 @@ RELATED_IMAGES=$(cat helm/install/values.yaml | shyaml values relatedImages imag
 echo $GCR_CREDENTIALS | base64 -d > keyfile.json
 docker login -u _json_key --password-stdin https://gcr.io < keyfile.json
 echo $HARBOR_PASSWORD | docker login -u $HARBOR_USERNAME --password-stdin https://harbor.devops.indico.io
-echo $CRUNCHY_PASSWORD | docker login -u $CRUNCHY_USERNAME --password-stdin $CRUNCHY_REGISTRY
+echo $CRUNCHY_PASSWORD | docker login -u $CRUNCHY_USERNAME --password-stdin https://$CRUNCHY_REGISTRY
 # pull images
 
 #pull postgres-operator controller image
 IMAGE_BASE=$(basename $CONTROLLER_IMAGE_CLUSTER)
-docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
+PRIVATE_IMAGE=$CRUNCHY_REGISTRY/crunchydata/$IMAGE_BASE
+docker pull $PRIVATE_IMAGE
 #push to gcr
-docker tag $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE
+docker tag $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE
 docker push gcr.io/new-indico/$IMAGE_BASE
 #push to harbor
-docker tag $CONTROLLER_IMAGE_CLUSTER harbor.devops.indico.io/indico/$IMAGE_BASE
+docker tag $PRIVATE_IMAGE harbor.devops.indico.io/indico/$IMAGE_BASE
 docker push harbor.devops.indico.io/indico/$IMAGE_BASE
 #Remove Junk
-docker rmi $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker rmi $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
 
 #pull postgres-operator upgrade image
-IMAGE_BASE=$(basename $CONTROLLER_IMAGE_UPGRADE)
-docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
+IMAGE_BASE=$(basename $CONTROLLER_IMAGE_UPGRADE
+PRIVATE_IMAGE=$CRUNCHY_REGISTRY/crunchydata/$IMAGE_BASE
+docker pull $PRIVATE_IMAGE
 #push to gcr
-docker tag $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE
+docker tag $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE
 docker push gcr.io/new-indico/$IMAGE_BASE
 #push to harbor
-docker tag $CONTROLLER_IMAGE_UPGRADE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker tag $PRIVATE_IMAGE harbor.devops.indico.io/indico/$IMAGE_BASE
 docker push harbor.devops.indico.io/indico/$IMAGE_BASE
 #Remove Junk
-docker rmi $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker rmi $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
 
 for ri in $RELATED_IMAGES;
 do
     IMAGE=$ri
     IMAGE_BASE=$(basename $IMAGE)
-    docker pull $CRUNCHY_REGISTRY/$IMAGE_BASE
+    PRIVATE_IMAGE=$CRUNCHY_REGISTRY/crunchydata/$IMAGE_BASE
+    docker pull $PRIVATE_IMAGE
     #push to gcr
-    docker tag $IMAGE gcr.io/new-indico/$IMAGE_BASE
+    docker tag $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE
     docker push gcr.io/new-indico/$IMAGE_BASE
     #push to harbor
-    docker tag $IMAGE harbor.devops.indico.io/indico/$IMAGE_BASE
+    docker tag $PRIVATE_IMAGE harbor.devops.indico.io/indico/$IMAGE_BASE
     docker push harbor.devops.indico.io/indico/$IMAGE_BASE
     #Remove Junk
-    docker rmi $IMAGE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
+    docker rmi $PRIVATE_IMAGE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
 done
 
